831d33d53d7802242abb1003dc261a8638e82f42
stergiosbamp <bampakis.stergios@gmail.com>
2020-12-19 13:36:15 +0200

    Refactored correlated distribution script

--- data/correlation.py
+++ data/correlation.py
@@ -1,17 +1,27 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 from numpy.random import default_rng
 
 
 NUM_SAMPLES = 1000
-DIM = 2
 
 rng = default_rng(1)
 
-# prepare data
-data1 = 20 * rng.standard_normal(size=(NUM_SAMPLES, DIM)) + 100
-data2 = data1 + (10 * rng.standard_normal(size=(NUM_SAMPLES, DIM)) + 50)
+# The desired mean values
+mean = [1, 1]
+# The desired covariance matrix
+cov = [
+    [3.5, 2.6],
+    [2.6, 3.5]
+]
+
+# Generate the random samples.
+x = rng.multivariate_normal(mean, cov, size=NUM_SAMPLES)
 
 # plot
-plt.scatter(data1, data2)
+plt.scatter(x[:, 0], x[:, 1])
 plt.show()
+
+# save the data into csv file with 5 decimals
+np.savetxt("anticorrelation.csv", x, delimiter=",", fmt="%.5f")
