a0dd93b4bdf4c2473f25807271d3cc807b986f5a
stergiosbamp <bampakis.stergios@gmail.com>
2020-12-20 21:18:33 +0200

    WIP: Sfs algorithm

--- spark-app/src/main/scala/Main.scala
+++ spark-app/src/main/scala/Main.scala
@@ -1,5 +1,6 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.Row
 
 object Main {
 
@@ -10,12 +11,18 @@ object Main {
       .getOrCreate()
 
     val PARTITIONS = 3
+    val sfs = SFS
+    var globalSkyline: Set[Row] = Set()
 
     val df = spark.read.csv("src/main/resources/mock-datapoints.csv")
 
     val sumDF = df.withColumn("sum", df.columns.map(c => col(c)).reduce((c1, c2) => c1 + c2))
     val sortedSumDF = sumDF.sort(col("sum").asc)
 
+    // First point of the sorted df is in the skyline
+    globalSkyline += sortedSumDF.first()
+
+    println(globalSkyline)
 
   }
 
---
+++ spark-app/src/main/scala/SFS.scala
@@ -0,0 +1,11 @@
+import org.apache.spark.sql.{DataFrame, Row}
+
+object SFS {
+
+  def computeLocalSkyline(df: DataFrame): Set[Row] = {
+    var localSkyline: Set[Row] = Set()
+
+
+    localSkyline
+  }
+}
