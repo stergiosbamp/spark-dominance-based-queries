3254ffb8002209491b97dec0bfa2813bcfd699b2
Themistoklis Spanoudis <themistoklis.spanoudis@gmail.com>
2020-12-21 14:43:55 +0200

    Added inferSchema option to read csv coordinates as double, not string.

--- spark-app/src/main/scala/Main.scala
+++ spark-app/src/main/scala/Main.scala
@@ -13,10 +13,14 @@ object Main {
     val sfs = SFS
     var globalSkyline: Set[Row] = Set()
 
-    val df = spark.read.csv("src/main/resources/mock-datapoints.csv")
+    val df = spark.read.option("inferSchema", "true").csv("src/main/resources/mock-datapoints.csv")
 
     val sumDF = df.withColumn("sum", df.columns.map(c => col(c)).reduce((c1, c2) => c1 + c2))
     val sortedSumDF = sumDF.sort(col("sum").asc)
+
+    df.printSchema()
+    sumDF.printSchema()
+    sortedSumDF.printSchema()
 
     // First point of the sorted df is in the skyline
     globalSkyline += sortedSumDF.first()
