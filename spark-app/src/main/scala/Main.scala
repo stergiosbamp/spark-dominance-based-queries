dfa7522fb8657585fb51e17a4f321dbadab46520
Themistoklis Spanoudis <themistoklis.spanoudis@gmail.com>
2020-12-22 21:41:15 +0200

    Added accumulator, refactored SFS.

--- spark-app/src/main/scala/Main.scala
+++ spark-app/src/main/scala/Main.scala
@@ -2,6 +2,7 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.Row
 
+
 object Main {
 
   def main(args: Array[String]): Unit = {
@@ -11,21 +12,24 @@ object Main {
       .getOrCreate()
 
     val sfs = SFS
-    var globalSkyline: Set[Row] = Set()
+    // var globalSkyline: Set[Row] = Set()
 
     val df = spark.read.option("inferSchema", "true").csv("src/main/resources/mock-datapoints.csv")
+
+    val localSkylinesAcc = spark.sparkContext.collectionAccumulator[Row]("localSkylinesAcc")
 
     val sumDF = df.withColumn("sum", df.columns.map(c => col(c)).reduce((c1, c2) => c1 + c2))
     val sortedSumDF = sumDF.sort(col("sum").asc)
 
-    df.printSchema()
-    sumDF.printSchema()
-    sortedSumDF.printSchema()
+    // df.printSchema()
+    // sumDF.printSchema()
+    // sortedSumDF.printSchema()
 
     // First point of the sorted df is in the skyline
-    globalSkyline += sortedSumDF.first()
-    sfs.computeLocalSkyline(sortedSumDF)
-    println(globalSkyline)
+    // globalSkyline += sortedSumDF.first()
+    sfs.computeLocalSkyline(sortedSumDF, localSkylinesAcc)
+    // println(globalSkyline)
+    println(localSkylinesAcc)
 
   }
 
--- spark-app/src/main/scala/SFS.scala
+++ spark-app/src/main/scala/SFS.scala
@@ -1,31 +1,59 @@
 import org.apache.spark.sql.{DataFrame, Row}
-
-import scala.math.BigDecimal.double2bigDecimal
-
+import org.apache.spark.util.CollectionAccumulator
+import scala.collection.mutable.Set
 
 object SFS {
 
-  def computeLocalSkyline(df: DataFrame): Seq[Row] = {
-    var localSkyline = Seq[Row]()
+  def computeLocalSkyline(df: DataFrame, localSkylinesAcc: CollectionAccumulator[Row]): Set[Row] = {
 
-    df.foreach(row => {
-      val x = row.getDouble(0)
-      val y = row.getDouble(1)
+    println("Called computeLocalSkyline", df)
+
+    var localSkyline = Set[Row]()
+
+    def addOrIgnoreCandidateRow(candidateRow: Row, localSkyline: Set[Row]): Unit = {
+
+      // The following line is just to demonstrate that the CollectionAccumulator can indeed collect the rows.
+      // localSkylinesAcc.add(candidateRow)
+
+      // The problem is that the localSkyline variable doesn't keep the state that we need (the rows that belong to the
+      // local skyline. This can be seen by the relevant prints.
+
+      println("Called addOrIngoreCandidateRow", candidateRow)
 
       if (localSkyline.isEmpty) {
-        localSkyline = localSkyline :+ row
+        println("LocalSkyline is empty", localSkyline)
+        localSkyline += candidateRow
+        println("Added best local row to skyline", candidateRow)
+        println("Now skyline has one row", localSkyline)
       }
 
-      localSkyline.foreach{ element => {
-        val xSkyline = element.getDouble(0)
-        val ySkyline = element.getDouble(1)
-        if ((x <= xSkyline && y < ySkyline) || (x <= ySkyline && y < ySkyline)) {
-          localSkyline = localSkyline :+ row
+      var isDominated = false
+
+      def checkCandidateRowAgainstSkylineRow(candidateRow: Row, skylineRow: Row): Unit = {
+        val x = candidateRow.getDouble(0)
+        val y = candidateRow.getDouble(1)
+
+        val xSkyline = skylineRow.getDouble(0)
+        val ySkyline = skylineRow.getDouble(1)
+
+        if ((xSkyline < x && ySkyline <= y) || (xSkyline <= x && ySkyline < y)) {
+          isDominated = true
         }
-      }}
-    })
+      }
+
+      localSkyline.foreach{skylineRow => checkCandidateRowAgainstSkylineRow(candidateRow, skylineRow)}
+
+      if (!isDominated) {
+        localSkyline += candidateRow
+      }
+    }
+
+    df.foreach(row => addOrIgnoreCandidateRow(row, localSkyline))
+
+    localSkyline.foreach(row => localSkylinesAcc.add(row))
 
     println("INSIDE:", localSkyline)
+
     localSkyline
   }
 }
