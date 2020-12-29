8a81d06b8a889e13a8e15a1e4cc7cc4f8efdd190
Themistoklis Spanoudis <themistoklis.spanoudis@gmail.com>
2020-12-29 02:23:12 +0200

    Added scripts for plot generation.

---
+++ plots/runtime_vs_topk.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+
+# top-k
+topk = [10, 20, 40]
+
+# runtimes for correlated (task 2 -> top-k dominating)
+topk_corr = [26.97, 50.03, 134.83]
+
+# runtimes for uniform (task 2 -> top-k dominating)
+topk_uni = [36.04, 75.79, 210.62]
+
+# runtimes for normal (task 2 -> top-k dominating)
+topk_norm = [43.89, 84.38, 239.98]
+
+# runtimes for anticorrelated (task 2 -> top-k dominating)
+topk_anti = [70.31, 152.12, 451.77]
+
+# plot options
+xlabel = "Top-k"
+ylabel = "Execution time [s]"
+
+# plot runtimes for correlated distributions
+plt.figure()
+plt.title("Correlated distribution")
+plt.xlabel(xlabel)
+plt.ylabel(ylabel)
+plt.plot(topk, topk_corr)
+plt.legend(["Top-k dominating"])
+
+# plot runtimes for correlated distributions
+plt.figure()
+plt.title("Uniform distribution")
+plt.xlabel(xlabel)
+plt.ylabel(ylabel)
+plt.plot(topk, topk_uni)
+plt.legend(["Top-k dominating"])
+
+# plot runtimes for correlated distributions
+plt.figure()
+plt.title("Normal distribution")
+plt.xlabel(xlabel)
+plt.ylabel(ylabel)
+plt.plot(topk, topk_norm)
+plt.legend(["Top-k dominating"])
+
+# plot runtimes for correlated distributions
+plt.figure()
+plt.title("Anticorrelated distribution")
+plt.xlabel(xlabel)
+plt.ylabel(ylabel)
+plt.plot(topk, topk_anti)
+plt.legend(["Top-k dominating"])
+
+plt.show()
