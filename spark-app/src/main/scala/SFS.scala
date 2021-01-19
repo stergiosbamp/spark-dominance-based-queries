c51152c40a00def4b011e8ae89eb2a251e9c8454
stergiosbamp <bampakis.stergios@gmail.com>
2021-01-19 22:07:39 +0200

    Refactor skyline query with local skyline per partition

--- spark-app/src/main/scala/SFS.scala
+++ spark-app/src/main/scala/SFS.scala
@@ -1,44 +1,99 @@
-import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, Dataset, Row}
 import org.apache.spark.util.CollectionAccumulator
+
+import scala.collection.mutable.ArrayBuffer
 
 
 object SFS {
 
-  def computeLocalSkyline(df: DataFrame, localSkylinesAcc: CollectionAccumulator[Row]): Unit = {
+  def computeFinalSkyline(skylineAccumulator: CollectionAccumulator[Row]): ArrayBuffer[Row] = {
+    var sortedPoints = new ArrayBuffer[Row]()
+    skylineAccumulator.value.forEach( r => {
+      sortedPoints += r
+    })
+    sortedPoints = sortedPoints.sortBy(r => r.getDouble(r.length - 1))
 
-    df.coalesce(1).foreach( r => {
+    var finalSkyline = new ArrayBuffer[Row]()
 
+    sortedPoints.foreach( r => {
       val dimensions = r.length - 1
 
       // Array that represents the dimensions (coordinates) of the examined point
-      val pointDimensions = Array.fill(dimensions){0.0}
-      for ( i <- 0 until dimensions) {
+      val pointDimensions = Array.fill(dimensions) {
+        0.0
+      }
+      for (i <- 0 until dimensions) {
         pointDimensions(i) += r.getDouble(i)
       }
 
-      val value = localSkylinesAcc.value
-
       var isDominated = false
-      value.forEach(skylineRow => {
+
+      for (x <- finalSkyline) {
 
         // Array that represents the dimensions (coordinates) of the skyline point
-        val skylineDimensions = Array.fill(dimensions){0.0}
-        for ( i <- 0 until dimensions) {
-          skylineDimensions(i) += skylineRow.getDouble(i)
+        val skylineDimensions = Array.fill(dimensions) {
+          0.0
+        }
+        for (i <- 0 until dimensions) {
+          skylineDimensions(i) += x.getDouble(i)
         }
 
-        if (isMultidimensionalPointDominated(dimensions, pointDimensions, skylineDimensions)){
+        if (isMultidimensionalPointDominated(dimensions, pointDimensions, skylineDimensions)) {
           isDominated = true
         }
+      }
 
-      })
-
-      if (!isDominated){
-        // add the coords of the points excluding the sum
-        localSkylinesAcc.add(r)
+      if (!isDominated) {
+        finalSkyline += r
       }
     })
+    finalSkyline
+  }
 
+  def computeLocalSkyline(df: DataFrame, skylineAccumulator: CollectionAccumulator[Row]): Unit = {
+    val rowsRDD: RDD[Row] = df.rdd
+
+    rowsRDD.foreachPartition( iterator => {
+
+      var localSkyline = new ArrayBuffer[Row]()
+
+      iterator.foreach(row => {
+        val dimensions = row.length - 1
+
+        // Array that represents the dimensions (coordinates) of the examined point
+        val pointDimensions = Array.fill(dimensions) {
+          0.0
+        }
+        for (i <- 0 until dimensions) {
+          pointDimensions(i) += row.getDouble(i)
+        }
+
+        var isDominated = false
+
+        for (x <- localSkyline) {
+
+          // Array that represents the dimensions (coordinates) of the skyline point
+          val skylineDimensions = Array.fill(dimensions) {
+            0.0
+          }
+          for (i <- 0 until dimensions) {
+            skylineDimensions(i) += x.getDouble(i)
+          }
+
+          if (isMultidimensionalPointDominated(dimensions, pointDimensions, skylineDimensions)) {
+            isDominated = true
+          }
+        }
+
+        if (!isDominated) {
+          localSkyline += row
+        }
+      })
+      localSkyline.foreach( r => {
+        skylineAccumulator.add(r)
+      })
+    })
   }
 
   /***
