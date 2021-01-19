87d4bd38963f5fc556f95ec82d378dcebb1cb506
stergiosbamp <bampakis.stergios@gmail.com>
2021-01-19 22:32:58 +0200

    Fixes for task 2,3 with the new implementation of skyline

--- spark-app/src/main/scala/Main.scala
+++ spark-app/src/main/scala/Main.scala
@@ -3,6 +3,7 @@ import java.util
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.apache.spark.sql.functions.col
 
+import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 import scala.util.control.Breaks.break
 
@@ -17,13 +18,13 @@ object Main {
       .appName("Skyline Dominance Spark app")
       .getOrCreate()
 
-    val df = spark.read.option("inferSchema", "true").csv("src/main/resources/mock-datapoints.csv")
+    val df = spark.read.option("inferSchema", "true").csv("../data/correlation-1000000.csv")
 
-    val skylineSet: ArrayBuffer[Row] = skylineQuery(spark, df)
+    val skylineSet = skylineQuery(spark, df)
     println(s"Skyline set is: ${skylineSet}")
 
 //    topKDominating(2, spark, df)
-
+//
 //    topKSkyline(2, spark, df)
 
     val duration = (System.nanoTime() - startTime) / 1e9d
@@ -40,68 +41,69 @@ object Main {
     val sfs = SFS
     sfs.computeLocalSkyline(sortedSumDF, skylineAccumulator)
     sfs.computeFinalSkyline(skylineAccumulator)
-
   }
 
-//  def topKDominating(k: Int, spark: SparkSession, df: DataFrame): Unit = {
-//    var changingDf = df
-//    for (i <- 1 to k) {
-//      val skylinePoints = skylineQuery(spark, changingDf)
-//      val domination = Domination
-//      val scoreAcc = spark.sparkContext.longAccumulator("Score accumulator")
-//      var dominatingMap = Map[Row, Long]()
-//      skylinePoints.forEach( row => {
-//        scoreAcc.reset()
-//        domination.dominantScore(row, changingDf, scoreAcc)
-//        dominatingMap = dominatingMap + (row -> scoreAcc.value)
-//      })
-//
-//      // sort points by the dominance score
-//      val sortedDominatingMap = dominatingMap.toSeq.sortWith(_._2 > _._2)
-//      val (topPoint, topValue) = sortedDominatingMap.head
-//      println(s"Top-$i is point $topPoint with dominance score $topValue")
-//
-//      // Remove top point from dataset and repeat
-//      changingDf = changingDf.filter(r => {
-//        val dimensions = r.length - 1
-//        val pointDimensions = Array.fill(dimensions){0.0}
-//        val topPointDimensions = Array.fill(dimensions){0.0}
-//
-//        for ( i <- 0 until dimensions) {
-//          pointDimensions(i) += r.getDouble(i)
-//          topPointDimensions(i) += topPoint.getDouble(i)
-//        }
-//
-//        !pointDimensions.sameElements(topPointDimensions)
-//      })
-//    }
-//  }
-//
-//  def topKSkyline(k: Int, spark: SparkSession, df: DataFrame): Unit = {
-//    val skylinePoints = skylineQuery(spark, df)
-//    val domination = Domination
-//    val scoreAcc = spark.sparkContext.longAccumulator("Score accumulator")
-//    var dominatingMap = Map[Row, Long]()
-//    skylinePoints.forEach( row => {
-//      scoreAcc.reset()
-//      domination.dominantScore(row, df, scoreAcc)
-//      dominatingMap = dominatingMap + (row -> scoreAcc.value)
-//    })
-//
-//    // sort points by the dominance score
-//    val sortedDominatingMap = dominatingMap.toSeq.sortWith(_._2 > _._2)
-//    val numOfElements = sortedDominatingMap.size
-//
-//    var i = 1
-//    for ((topK, topV) <- sortedDominatingMap) {
-//      println(s"Top-$i is point $topK with dominance score $topV")
-//      i = i + 1
-//      if (i > k){
-//        return
-//      }
-//    }
-//    if (k > numOfElements) {
-//      println(s"No other points exist in the skyline. Request a lower k value until $numOfElements")
-//    }
-//  }
+  def topKDominating(k: Int, spark: SparkSession, df: DataFrame): Unit = {
+    var changingDf = df
+    for (i <- 1 to k) {
+      val skylinePoints: ArrayBuffer[Row] = skylineQuery(spark, changingDf)
+      val domination = Domination
+      val scoreAcc = spark.sparkContext.longAccumulator("Score accumulator")
+      var dominatingMap = Map[Row, Long]()
+
+      for (row <- skylinePoints) {
+        scoreAcc.reset()
+        domination.dominantScore(row, changingDf, scoreAcc)
+        dominatingMap = dominatingMap + (row -> scoreAcc.value)
+      }
+
+      // sort points by the dominance score
+      val sortedDominatingMap = dominatingMap.toSeq.sortWith(_._2 > _._2)
+      val (topPoint, topValue) = sortedDominatingMap.head
+      println(s"Top-$i is point $topPoint with dominance score $topValue")
+
+      // Remove top point from dataset and repeat
+      changingDf = changingDf.filter(r => {
+        val dimensions = r.length - 1
+        val pointDimensions = Array.fill(dimensions){0.0}
+        val topPointDimensions = Array.fill(dimensions){0.0}
+
+        for ( i <- 0 until dimensions) {
+          pointDimensions(i) += r.getDouble(i)
+          topPointDimensions(i) += topPoint.getDouble(i)
+        }
+
+        !pointDimensions.sameElements(topPointDimensions)
+      })
+    }
+  }
+
+  def topKSkyline(k: Int, spark: SparkSession, df: DataFrame): Unit = {
+    val skylinePoints = skylineQuery(spark, df)
+    val domination = Domination
+    val scoreAcc = spark.sparkContext.longAccumulator("Score accumulator")
+    var dominatingMap = Map[Row, Long]()
+
+    for (row <- skylinePoints) {
+      scoreAcc.reset()
+      domination.dominantScore(row, df, scoreAcc)
+      dominatingMap = dominatingMap + (row -> scoreAcc.value)
+    }
+
+    // sort points by the dominance score
+    val sortedDominatingMap = dominatingMap.toSeq.sortWith(_._2 > _._2)
+    val numOfElements = sortedDominatingMap.size
+
+    var i = 1
+    for ((topK, topV) <- sortedDominatingMap) {
+      println(s"Top-$i is point $topK with dominance score $topV")
+      i = i + 1
+      if (i > k){
+        return
+      }
+    }
+    if (k > numOfElements) {
+      println(s"No other points exist in the skyline. Request a lower k value until $numOfElements")
+    }
+  }
 }
